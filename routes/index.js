15876bff6e43d9444ef0d923c2b181933137499f
Ocean <s@ocean.lol>
2021-07-17 19:54:30 +0100

    slash

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,10 @@
 var router = require('express').Router();
-const slash = require('express-slash');
 
 //function to require authentication for a route
 const requireAuth = require('./requireAuth');
 
 router.use('/', require('./auth'));
 router.use('/', require('./home'));
-
-router.use(slash());
 
 router.use('/groups/', requireAuth, require('./groups'));
 router.use('/g/:groupID/', requireAuth, require('./group'));
